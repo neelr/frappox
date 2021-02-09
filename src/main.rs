c47b60bbe82bb6e3eb794d816cb94c45c1cf39b2
Neel Redkar <neel.redkar@gmail.com>
2021-02-09 11:27:44 -0800

    init

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+   let num = std::env::args().nth(1).expect("no number given");
+   let result = approx(num.parse::<f64>().unwrap());
+   println!("{}/{}",result.0,result.1);
+}
+
+pub fn approx(num :f64) -> (i64,i64) {
+    let decimal = num - (num as i64) as f64;
+    let mut denomin = (0,1);
+    let mut denomax = (1,1);
+    let mut iter = 0;
+    let mut mediant = ((denomin.0 + denomax.0) as f64)/((denomin.1 + denomax.1) as f64);
+    while iter < 10000000 &&  mediant != decimal {
+        if decimal > mediant {
+            denomin = (denomin.0 + denomax.0, denomin.1 + denomax.1);
+        } else {
+            denomax = (denomin.0 + denomax.0, denomin.1 + denomax.1);
+        }
+        iter+=1;
+        mediant =  ((denomin.0 + denomax.0) as f64)/((denomin.1 + denomax.1) as f64);
+    }
+    return ((denomin.0 + denomax.0) + (num as i64)*(denomin.1 + denomax.1), denomin.1 + denomax.1);
+}
